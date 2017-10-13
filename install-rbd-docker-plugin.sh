37a41e8af2310e6f71595bcc809e6dc12260aee2
xingjiudong <25635680@qq.com>
2017-10-13 16:06:03 +0800

    Fix image

--- install-rbd-docker-plugin.sh
+++ install-rbd-docker-plugin.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v /opt/bin:/tmp/plugin xingjiudong/rbd-docker-plugin cp /go/bin/rbd-docker-plugin /tmp/plugin/rbd-docker-plugin
+docker run --rm -v /opt/bin:/tmp/plugin openfrontier/rbd-docker-plugin cp /go/bin/rbd-docker-plugin /tmp/plugin/rbd-docker-plugin
